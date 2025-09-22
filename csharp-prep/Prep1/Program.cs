9230855a022309599af99741924046f9a33958ee
Taylor Peless <pel24008@byui.edu>
2025-09-22 17:11:16 -0600

    Edited the file to print a name like "Bond, James Bond" after user input of the first and last names.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,13 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        var firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        var lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
