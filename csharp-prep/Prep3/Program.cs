65d59797512c8f6bc356aa4205374f02e642ac4b
Taylor Peless <pel24008@byui.edu>
2025-09-22 18:06:55 -0600

    Finished part 3 of the preparation.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        var random = new Random();
+
+        string escapeString = "yes";
+
+        while (escapeString.ToLower().Equals("yes"))
+        {
+            int guess = 0;
+            int guessCount = 0;
+            int magicNumber = random.Next(1, 11);
+
+            while (guess != magicNumber)
+            {
+                Console.Write("\nWhat do you think the magic number is? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+                
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine($"You got it in {guessCount} guesses!\n");
+                    break;
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }       
+            }
+
+            Console.Write("Do you want to play again? Enter Yes to play again or No to quit: ");
+            escapeString = Console.ReadLine();
+        }        
     }
 }
